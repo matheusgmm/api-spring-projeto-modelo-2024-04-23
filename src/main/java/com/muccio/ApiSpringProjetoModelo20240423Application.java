c789c79599cdf3edf3f9d3449a140c22c8be8576
Matheus Muccio <matheus.gmuccio@gmail.com>
2024-04-23 13:48:07 -0300

    Iniciando projeto

---
+++ src/main/java/com/muccio/ApiSpringProjetoModelo20240423Application.java
@@ -0,0 +1,13 @@
+package com.muccio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringProjetoModelo20240423Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringProjetoModelo20240423Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/muccio/ApiSpringProjetoModelo20240423ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muccio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringProjetoModelo20240423ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
